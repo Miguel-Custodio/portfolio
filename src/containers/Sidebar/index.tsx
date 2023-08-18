73e440a8cc29bc8ac1eddfaaff5c23bfad597dd5
Miguel-Custodio <repmiguelito@gmail.com>
2023-08-18 10:48:38 -0300

    construindo a estrutura e a seção 'sobre mim'

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/miguel-custodio.png" />
+    <Titulo fontSize={16}>Miguel Custodio</Titulo>
+  </aside>
+)
+
+export default Sidebar
